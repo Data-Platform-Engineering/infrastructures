17b17f98d5b61bc62932cf94c20e03010589e1d1
Chidera Ozigbo <chideraozigbo@gmail.com>
2025-07-31 17:05:58 +0100

    fix: changed s3 bucket backend name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "finance-n8n-automation"
+    bucket       = "airflow-infra-prod"
     key          = "state/terraform.tfstate"
     region       = "eu-north-1"
     use_lockfile = true
