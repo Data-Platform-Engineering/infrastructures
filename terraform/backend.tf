c0489888cc595e8bc50361110547210683de6cb2
Chidera Ozigbo <chideraozigbo@gmail.com>
2025-08-02 15:33:59 +0100

    fix: cleanup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "airflow-infra-prod"
-    key          = "state/terraform.tfstate"
+    bucket       = "dpe-terraform-state"
+    key          = "production/production.tfstate"
     region       = "eu-north-1"
     use_lockfile = true
     encrypt      = true
